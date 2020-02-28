ed7425d807cfa0ce82bc7443f7de2fbd6ad0e506
davinchick <57950327+davinchick@users.noreply.github.com>
2020-02-28 15:32:40 +0300

    Fix: tests are passe

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,17 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+    if (!date) { return 'Unable to determine the time of year!';}
+    if (Object.keys(date).length > 0) { throw new Error();}
+    let fourSeasons = {
+        winter: [11, 0, 1],
+        spring: [2, 3, 4],
+        summer: [5, 6, 7],
+        autumn: [8, 9, 10]
+    };
+    let month = date.getMonth();
+
+    for (let season in fourSeasons) {
+        if (fourSeasons[season].includes(month)) {
+            return season;
+        }
+    }
 };
