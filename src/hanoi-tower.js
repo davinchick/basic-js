4a8797087defcaef90e488f32b9c42126d238081
davinchick <57950327+davinchick@users.noreply.github.com>
2020-02-28 14:36:13 +0300

    Fix: tests are passe

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-}
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    var turn = Math.pow(2, disksNumber)-1;
+    var second = turn/(turnsSpeed / 3600);
+
+    return {
+        turns: turn,
+        seconds: second
+    };
+};
