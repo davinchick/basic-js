52b7458ced495f48bddd381f18be9c1292dbe4be
davinchick <57950327+davinchick@users.noreply.github.com>
2020-02-28 14:35:55 +0300

    Fix: tests are passe

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,12 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(string, options ) {
+    if(typeof string !== 'string'){ string = string+'';}
+    let rep = 1, addTime = 1, sep = '+', add = '', addSep = '|';
+
+    if(typeof options.repeatTimes === 'number'){ rep = options.repeatTimes;}
+    if(typeof options.separator === 'string'){   sep = options.separator;}
+    if(typeof options.addition !== 'undefined' && typeof options.addition !== 'null'){ add = String(options.addition);}
+    if(typeof options.additionRepeatTimes === 'number'){ addTime = options.additionRepeatTimes;}
+    if(typeof options.additionSeparator === 'string'){   addSep = options.additionSeparator;}
+
+    return (string+((add+ addSep).repeat(addTime-1)+add)+sep).repeat(rep-1)+ string + (add+ addSep).repeat(addTime-1)+add;
 };
-  
