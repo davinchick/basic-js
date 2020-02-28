f60f4eebcc8338f7669bd4594848fa6ec6525878
davinchick <57950327+davinchick@users.noreply.github.com>
2020-02-28 14:34:16 +0300

    Fix: tests are passed

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(array) {
+    var count = 0;
+    for (var i=0; i<array.length; i++){
+        for(var j=0; j<array[i].length; j++){
+            if(array[i][j]==='^^'){
+                count++;
+            }
+        }
+    }
+    return count;
 };
