2fb5f484ed51952c74a1110a71ed62b1935d9962
davinchick <57950327+davinchick@users.noreply.github.com>
2020-02-28 14:35:20 +0300

    Fix: tests are passed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+    var secretName = '';
+    if(Array.isArray(array)){
+        for(var i=0; i<array.length; i++){
+            if(typeof array[i] === "string"){
+                secretName+= array[i].replace(/\s+/g,'').charAt(0);}
+        }
+        return secretName.toUpperCase().split('').sort().join('');}
+    return false;
 };
